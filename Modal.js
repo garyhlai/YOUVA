1936224f01fbb8c40401e7b24684aab4ba62a111
Gary Lai <garylai@dyn-129-236-208-160.dyn.columbia.edu>
2018-11-16 21:27:21 -0500

    updates the pages, fixes Modal functionality

--- Modal.js
+++ Modal.js
@@ -2,17 +2,59 @@
 var modal = document.getElementById('myModal');
 
 //get the image and insert it inside the modal
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById('img01');
+//var img = document.getElementById('myImg');
+var modalImg = document.getElementById('img');
 var captionText = document.getElementById("caption");
 var subCaption;
+var currentName; 
 
-img.onclick = function(){
+var counter;
+var numberTracker;
+var currentNumber;
+var nextNumber;
+var nextName;
+var prevNumber;
+var prevName;
+
+
+
+var next = document.getElementById('next');
+var prev = document.getElementById('prev');
+
+$('.myImg').click(function(){
 	modal.style.display = "block";
+	//modalImg = document.getElementById('img');
 	modalImg.src = this.src;
+	currentName = modalImg.src;
 	captionText.innerHTML = this.alt;
 	subCaption = this.alt;
-}
+
+
+
+
+
+
+	//get the substring you're aiming at 
+	counter = 0;
+	for (var i=1; i<currentName.length; i++){
+		//get the number
+		if (currentName[(currentName.length-counter)] != '_')
+		{
+			counter+=1;
+		}
+	}
+
+//from currentName[(currentName.length-(counter-1))] to the end is the "number.jpg" string you want to change
+//numberTracker is usually constant 
+	numberTracker = currentName.length-(counter-1);
+	currentNumber = currentName.substring(numberTracker,(currentName.length-4));
+	nextNumber = Number(currentNumber) + 1;
+	nextName = currentName.substring(0,numberTracker) + nextNumber + ".jpg";
+	prevNumber = Number(currentNumber) - 1;
+	prevName = currentName.substring(0,numberTracker) + prevNumber + ".jpg";
+
+
+});
 
 
 
@@ -22,34 +64,6 @@ var span = document.getElementById("close");
 span.onclick = function() {
 	modal.style.display = "none";
 }
-
-
-
-var next = document.getElementById('next');
-var prev = document.getElementById('prev');
-var currentName = img.src;
-
-//get the substring you're aiming at 
-
-var counter = 0;
-for (var i=1; i<currentName.length; i++){
-	
-	//get the number
-	if (currentName[(currentName.length-counter)] != '_')
-	{
-		counter+=1;
-	}
-}
-
-
-//from currentName[(currentName.length-(counter-1))] to the end is the "number.jpg" string you want to change
-//numberTracker is usually constant 
-var numberTracker = currentName.length-(counter-1);
-var currentNumber = currentName.substring(numberTracker,(currentName.length-4));
-var nextNumber = Number(currentNumber) + 1;
-var nextName = currentName.substring(0,numberTracker) + nextNumber + ".jpg";
-var prevNumber = Number(currentNumber) - 1;
-var prevName = currentName.substring(0,numberTracker) + prevNumber + ".jpg";
 
 
 next.onclick = function() {
